0ab8ce18eaa4f864567ac3f9584534aff364073d
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2024-03-03 17:11:51 +0100

    start screen looks like on figma now

--- shoppingList/components/StartScreen.tsx
+++ shoppingList/components/StartScreen.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
 import Svg, { Path, Circle } from 'react-native-svg';
 
+const tags = ['Other', 'University', 'Shopping Lists', 'Important'];
+const todoLists = ["List 1", "List 2", "List 3", "List 4", "List 5"];
+
+const fontFamily = 'Nunito-Regular';
+// const fontFamily = 'Roboto-Regular';
+
 const HomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
@@ -10,41 +16,53 @@ const HomeScreen: React.FC = () => {
       <View style={styles.topRow}>
         {/* Settings Icon */}
         <TouchableOpacity onPress={() => console.log("Settings pressed")}>
-            <Svg width="20" height="20" fill="none" viewBox="0 0 19 19">
+            <Svg width="25" height="25" fill="none" viewBox="0 0 21 21" transform="translate(0, 1)">
                 <Path fill="#7CEFD3" d="M9.5 5.646a3.854 3.854 0 1 0 0 7.708 3.854 3.854 0 0 0 0-7.708ZM6.833 9.5a2.667 2.667 0 1 1 5.334 0 2.667 2.667 0 0 1-5.334 0Z"/>
                 <Path fill="#7CEFD3" d="M11.633 1.595c-.627-2.127-3.64-2.127-4.266 0l-.112.378c-.186.634-.91.933-1.49.618l-.346-.189c-1.947-1.06-4.078 1.07-3.017 3.017l.189.346c.315.58.016 1.304-.618 1.49l-.378.112c-2.127.627-2.127 3.64 0 4.266l.378.112c.634.187.933.91.618 1.49l-.189.346c-1.06 1.947 1.07 4.078 3.017 3.017l.346-.189a1.037 1.037 0 0 1 1.49.618l.112.378c.627 2.127 3.64 2.127 4.266 0l.112-.378c.187-.634.91-.934 1.49-.618l.346.189c1.947 1.06 4.078-1.07 3.017-3.017l-.189-.346a1.036 1.036 0 0 1 .618-1.49l.378-.112c2.127-.627 2.127-3.64 0-4.266l-.378-.112a1.037 1.037 0 0 1-.618-1.49l.189-.346c1.06-1.947-1.07-4.078-3.017-3.017l-.346.189a1.036 1.036 0 0 1-1.49-.618l-.112-.378Zm-3.127.336c.292-.991 1.696-.991 1.988 0l.112.378a2.224 2.224 0 0 0 3.197 1.324l.346-.188c.908-.494 1.9.498 1.406 1.406l-.188.346a2.224 2.224 0 0 0 1.324 3.197l.378.112c.991.292.991 1.696 0 1.988l-.378.112a2.224 2.224 0 0 0-1.324 3.197l.188.346c.494.908-.498 1.9-1.406 1.406l-.346-.188a2.224 2.224 0 0 0-3.197 1.324l-.112.378c-.292.991-1.696.991-1.988 0l-.112-.378a2.224 2.224 0 0 0-3.197-1.324l-.346.188c-.908.494-1.9-.498-1.406-1.406l.188-.346a2.224 2.224 0 0 0-1.324-3.197l-.378-.112c-.991-.292-.991-1.696 0-1.988l.378-.112a2.224 2.224 0 0 0 1.324-3.197l-.188-.346c-.494-.908.498-1.9 1.406-1.406l.346.188A2.224 2.224 0 0 0 8.394 2.31l.112-.378Z"/>
             </Svg>
         </TouchableOpacity>
-        {/* Title */}
         <Text style={styles.title}>TO DO's</Text>
         <View style={styles.spacer} />
       </View>
 
       {/* Tags Row */}
       <View style={styles.tagsRow}>
-        <TouchableOpacity style={styles.tag}>
-          <Text>Other</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.tag}>
-          <Text>University</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.tag}>
-          <Text>Shopping Lists</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.tag}>
-          <Text>Important</Text>
-        </TouchableOpacity>
+        {tags.map((tag, index) => (
+          <TouchableOpacity key={index} style={styles.tag}>
+            <Text style={styles.tagText}>{tag}</Text>
+          </TouchableOpacity>
+        ))}
+        <TouchableOpacity key={"active"} style={[styles.tag, styles.selectedTag]}>
+            <Text style={[styles.tagText, styles.selectedTagText]}>{"active"}</Text>
+          </TouchableOpacity>
       </View>
 
       {/* TODO Lists */}
       <View style={styles.todoLists}>
-        {/* TODO List items will be rendered here */}
+        {todoLists.map((list, index) => (
+          <TouchableOpacity key={index} style={styles.list}>
+            {/* <View style={styles.list}> */}
+              <Svg width="18" height="18" fill="none" viewBox="0 0 16 16" transform="translate(0, 1)">
+                <Path fill="#ADADAD" fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5Zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5Zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5Zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clip-rule="evenodd"/>
+              </Svg>
+              <Text style={styles.listText}>{list}</Text>
+            {/* </View> */}
+          </TouchableOpacity>
+        ))}
       </View>
 
       {/* Add List Button */}
-      <TouchableOpacity style={styles.addButton} onPress={() => console.log("Add List pressed")}>
-        <Text style={styles.addButtonText}>Add List</Text>
-      </TouchableOpacity>
+      <View style={styles.bottomRow}>
+        <View style={styles.spacerBottomRow} />
+        <TouchableOpacity style={styles.addButton} onPress={() => console.log("Add List pressed")}>
+          <Svg width="40" height="40" viewBox="0 0 50 40">
+          <Path fill="#000" fill-rule="evenodd" d="M10 23a1 1 0 0 1 1-1h18a1 1 0 1 1 0 2H11a1 1 0 0 1-1-1Zm0-8a1 1 0 0 1 1-1h18a1 1 0 1 1 0 2H11a1 1 0 0 1-1-1Zm0-8a1 1 0 0 1 1-1h18a1 1 0 1 1 0 2H11a1 1 0 0 1-1-1ZM4 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" clip-rule="evenodd"/>
+            <Svg width="50" height="40" viewBox="0 0 40 40" transform="translate(30, 26)">
+              <Path fill="#020202" d="M6.5.214c.434 0 .786.352.786.786v4.714H12a.786.786 0 0 1 0 1.572H7.286V12a.786.786 0 0 1-1.572 0V7.286H1a.786.786 0 1 1 0-1.572h4.714V1c0-.434.352-.786.786-.786Z"/>
+            </Svg>
+          </Svg>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -71,42 +89,75 @@ const styles = StyleSheet.create({
     fontSize: 24,
     // fontWeight: 'bold',
     color: '#7cefd3',
-    fontFamily: 'Nunito-Regular',
-    // fontFamily: 'Roboto-Regular',
+    fontFamily: fontFamily,
   },
   spacer: {
     width: 24,
   },
   tagsRow: {
     flexDirection: 'row',
+    flexWrap: 'wrap',
     marginBottom: 20,
   },
   tag: {
-    backgroundColor: '#eaeaea',
+    backgroundColor: undefined,
+    borderWidth: 1,
+    borderColor: '#ADADAD',
     paddingHorizontal: 12,
-    paddingVertical: 8,
+    paddingVertical: 2,
     borderRadius: 20,
     marginRight: 10,
+    marginBottom: 10,
+  },
+  selectedTag: {
+    backgroundColor: '#7CEFD3',
+    borderColor: '#7CEFD3',
+  },
+  tagText: {
+    color: '#ADADAD',
+    fontSize: 14,
+    fontFamily: fontFamily,
+  },
+  selectedTagText: {
+    color: '#151515',
   },
   todoLists: {
     flex: 1,
     // Add styling for TODO lists container if needed
   },
+  list: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  listText: {
+    color: '#ADADAD',
+    fontSize: 18,
+    marginLeft: 20,
+    fontFamily: fontFamily,
+  },
+  bottomRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  spacerBottomRow: {
+    // flex: 1,
+    flexGrow: 1,
+    // borderColor: 'red',
+    // borderWidth: 1,
+  },
   addButton: {
-    backgroundColor: 'blue',
+    backgroundColor: '#7CEFD3',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 12,
-    borderRadius: 10,
-    position: 'absolute',
+    paddingVertical: 6,
+    paddingHorizontal: 21,
+    borderRadius: 100,
+    // position: 'absolute',
     bottom: 20,
-    left: 20,
-    right: 20,
-  },
-  addButtonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
+    // left: 20,
+    // right: 20,
   },
 });
 
